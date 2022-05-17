c418088e12774e27dd3a4b46a04812a98af844ac
HenrikDK2 <henrik265@outlook.dk>
2022-05-17 19:19:00 +0200

    Added node-fetch

--- netlify/functions/geolocation.ts
+++ netlify/functions/geolocation.ts
@@ -1,21 +1,29 @@
 import { Handler } from "@netlify/functions";
+import fetch from "node-fetch";
 
 const handler: Handler = async (event) => {
-  if (!event.queryStringParameters.ip) {
-    return { statusCode: 400, body: "ip parameter is required" };
+  try {
+    if (!event.queryStringParameters.ip) {
+      return { statusCode: 400, body: "ip parameter is required" };
+    }
+
+    const response = await fetch(
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.GEOLOCATION_APIKEY}&ip=${event.queryStringParameters.ip}`
+    );
+
+    if (!response.ok) return { statusCode: 400, body: response.statusText };
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Internal server error",
+    };
   }
-
-  const response = await fetch(
-    `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.GEOLOCATION_APIKEY}&ip=${event.queryStringParameters.ip}`
-  );
-
-  if (!response.ok) return { statusCode: 400, body: response.statusText };
-  const data = await response.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
 };
 
 export { handler };
