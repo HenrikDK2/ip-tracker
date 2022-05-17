52fa470fa9be47682e4e384fa07e6e3390a141de
HenrikDK2 <henrik265@outlook.dk>
2022-05-17 18:53:18 +0200

    Deploy to  repo

---
+++ netlify/functions/geolocation.ts
@@ -0,0 +1,28 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event) => {
+  try {
+    if (!event.queryStringParameters.ip) {
+      return { statusCode: 400, body: "ip parameter is required" };
+    }
+
+    const response = await fetch(
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.GEOLOCATION_APIKEY}&ip=${event.queryStringParameters.ip}`
+    );
+
+    if (!response.ok) return { statusCode: 400, body: response.statusText };
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Internal server error",
+    };
+  }
+};
+
+export { handler };
---
+++ src/main.ts
@@ -0,0 +1,86 @@
+import { Loader } from "@googlemaps/js-api-loader";
+import { GeolocationData } from ".";
+
+const mapOptions: google.maps.MapOptions = {
+  disableDefaultUI: true,
+  center: {
+    lat: 50,
+    lng: 0,
+  },
+  zoom: 4,
+};
+
+const searchForm = document.getElementById("form");
+let google: typeof globalThis.google;
+let marker: google.maps.Marker = null;
+let map: google.maps.Map;
+
+async function initMap() {
+  const loader = new Loader({
+    apiKey: process.env.GOOGLE_MAPS_APIKEY,
+    version: "weekly",
+    libraries: ["places"],
+  });
+
+  try {
+    google = await loader.load();
+
+    map = new google.maps.Map(
+      document.getElementsByClassName("map")[0] as HTMLElement,
+      mapOptions
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+searchForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const searchInput = document.getElementById("search") as HTMLInputElement;
+
+    const response = await fetch(
+      `/.netlify/functions/geolocation?ip=` + searchInput.value
+    );
+    if (!response.ok) throw response.statusText;
+
+    const data: GeolocationData = await response.json();
+    const pos = {
+      lng: parseFloat(data.longitude),
+      lat: parseFloat(data.latitude),
+    };
+
+    // Add information to info cards
+    const cards = Array.from(document.getElementsByClassName("info__text"));
+
+    // IP
+    cards[0].textContent = data.ip;
+
+    // Location
+    cards[1].textContent = data.city + ", " + data.country_name;
+
+    // Timezone
+    if (data.time_zone.offset > 0) {
+      cards[2].textContent = "UTC +" + data.time_zone.offset;
+    } else {
+      cards[2].textContent = "UTC " + data.time_zone.offset;
+    }
+
+    // ISP
+    cards[3].textContent = data.isp;
+
+    // Add marker
+    map.setZoom(10);
+    map.setCenter(pos);
+    if (!marker) {
+      marker = new google.maps.Marker();
+      marker.setMap(map);
+    }
+    marker.setPosition(pos);
+    marker.setTitle(data.isp);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+initMap();
